2384482849aab47fdc38dadcd6fc06caabbc94bf
NewtonGold <63900235+NewtonGold@users.noreply.github.com>
2022-01-19 22:13:09 +0000

    commit to Prototype (change .venv file)

--- Prototype/QUBO_Formulation.py
+++ Prototype/QUBO_Formulation.py
@@ -1,2 +1,13 @@
+def integer_to_binary(variables: list) -> list:
+    binary_variables = []
+    count = 0
+    for variable in variables:
+        coefficient = 2 ^ count
+        binary_variables.append(variable)
+        count += 1
+        if count == 8:
+            count = 0
+
+
 if __name__ == "__main__":
     print("hello world")
