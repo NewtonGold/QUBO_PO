9bb56c331ade3f843a0b33b1c8b9add538561a74
NewtonGold <63900235+NewtonGold@users.noreply.github.com>
2022-02-13 23:46:22 +0000

    logical OR penalty for multiple binary variables added

--- Final Project/QUBO_Formulation.py
+++ Final Project/QUBO_Formulation.py
@@ -12,7 +12,17 @@ def main():
     mew = [0.004798, 0.000659, 0.003174, 0.001377]
     l = Placeholder('l')
     #initailising binary variables in an array
-    x = initialise_binary_variables(N)
+    #initial = Array.create('x', shape=(N*7), vartype='BINARY')
+    initial = Array.create('x', shape=(3), vartype='BINARY')
+    print("marker 1")
+    penalty = or_penalty(initial)
+    print("marker 2")
+    f = open("or_penalty.txt", "w")
+    f.write(str(penalty))
+    f.close()
+    return 0
+    x = initialise_binary_variables(N, initial)
+
     #x = Array.fill(LogEncInteger('x', (0, 100)), shape=N)
     print(type(l))
 
@@ -77,27 +87,34 @@ def total_investment_constraint(x: list, p: int) -> int:
     constraint = p * ((100 - sum(x))**2)
     return constraint
 
-def cardinality_constraint(x: list, p: int):
-    constraint = 0
+def cardinality_constraint(x: list, p: int, K: int):
+    constraint = K
     count = 0
-    for variable in x:
-        if count == 6:
-            x.append(temp[i] * 37)
-            count = 0
-        else:
-            x.append(temp[i] * (2 ** count))
-            count = count + 1
+    for i in range(0, (len(x) - 7), 7):
+        constraint -= or_penalty(x[i:i+7])
+    constraint = (constraint ** 2) * p
 
-def initialise_binary_variables(N: int) -> list:
-    temp = Array.create('x', shape=(N*7), vartype='BINARY')
+def or_penalty(x: list) -> int:
+    if len(x) > 2:
+        print(len(x))
+        y=[]
+        for i in range(1, len(x)):
+            y.append(x[i]) 
+        current_penalty = or_penalty(y)
+        return (1 - x[0] - current_penalty + x[0] * current_penalty)
+    else:
+        print(len(x))
+        return (1 - x[0] - x[1] + (x[0] * x[1]))
+
+def initialise_binary_variables(N: int, initial: list) -> list:
     x = []
     count = 0
-    for i in range(len(temp)):
+    for i in range(len(initial)):
         if count == 6:
-            x.append(temp[i] * 37)
+            x.append(initial[i] * 37)
             count = 0
         else:
-            x.append(temp[i] * (2 ** count))
+            x.append(initial[i] * (2 ** count))
             count = count + 1
     return x
 
