e1fe7fbee7e238bd9906d7553c5d10934aecffc0
NewtonGold <63900235+NewtonGold@users.noreply.github.com>
2022-02-25 15:36:53 +0000

    Removed nested OR constraint, Implemented auxilary variables

--- Final Project/QUBO_Formulation.py
+++ Final Project/QUBO_Formulation.py
@@ -13,12 +13,23 @@ def main():
     mew = [0.004798, 0.000659, 0.003174, 0.001377]
     l = Placeholder('l')
     #initailising binary variables in an array
-    initial = Array.create('x', shape=(N*7), vartype='BINARY')
+    initial = Array.create('x', shape=((N+1)*7), vartype='BINARY')
+    aux = []
+    temp = []
+    initial_length = len(initial)
+    for i in range(initial_length-7, initial_length, 1):
+        aux.append(initial[i-1])
+    aux = Array(aux)
+
+    for i in range(0, initial_length - 7, 1):
+        temp.append(initial[i])
+    initial = Array(temp)
+    
     #initial = Array.create('x', shape=(3), vartype='BINARY')
     #f = open("or_penalty.txt", "w")
     #f.write(str(constraint))
     #f.close()
-    x = initialise_binary_variables(N, initial)
+    x = initialise_binary_variables(N, initial, aux)
 
     #x = Array.fill(LogEncInteger('x', (0, 100)), shape=N)
     #print(type(l))
@@ -26,14 +37,12 @@ def main():
     # creates the cardinality constrained model
     H1 = create_hamiltonian_1(x, N, l, sd)
     H2 = create_hamiltonian_2(x, N, l, mew)
-    model = create_cc_model(x, initial, H1, H2, K)
-    print("flag6")
-    qubo, offset = model.to_qubo()
-    pprint(qubo)
+    model = create_cc_model(x, aux, H1, H2, K)
+    qubo, offset = model.to_qubo(feed_dict={'l': 1})
     f = open("qubo.txt", "w")
-    f.write(pprint(qubo))
+    f.write(str(qubo))
     f.close()
-    return 0
+    #return 0
     
 
     #qubo, offset = model.to_qubo(feed_dict={'l': 0.5})
@@ -47,7 +56,7 @@ def main():
     # best_sample = min(decoded_samples, key=lambda x: x.energy)
     # print('The best solution is: ' + str(best_sample.sample))
 
-    for i in range(1, 10):
+    for i in range(10, 11):
         bqm = model.to_bqm(feed_dict={'l': i/10})
         sa = neal.SimulatedAnnealingSampler()
         sampleset = sa.sample(bqm, num_reads=10)
@@ -56,17 +65,14 @@ def main():
         best_sample = min(decoded_samples, key=lambda x: x.energy)
         print('The best solution is: ' + str(best_sample.sample))
 
-def create_cc_model(x: list, initial: list, H1: int, H2: int, K: int) -> list:
+def create_cc_model(x: list, aux: list, H1: int, H2: int, K: int) -> list:
     
     final_H = H1 - H2
-    final_H = add_constraints(x, initial, final_H, K)
-    print("flag4")
+    final_H = add_constraints(x, aux, final_H, K)
     model = final_H.compile()
-    print("flag5")
-    f = open("model.txt", "w")
-    f.write(pprint(model))
+    f = open("final_H.txt", "w")
+    f.write(str(final_H))
     f.close()
-    #print(type(model))
     return model
 
 def create_hamiltonian_1(x: list, N: int, l: int, sd: list) -> int:
@@ -86,49 +92,31 @@ def create_hamiltonian_2(x: list, N: int, l: int, mew: list) -> int:
     H2 = (1 - l) * H2
     return H2
 
-def add_constraints(x: list, initial: list,  final_H: int, K: int) -> int:
-    print("flag1")
-    final_H += cardinality_constraint(initial, 5, K)
-    print("flag2")
+def add_constraints(x: list, aux: list,  final_H: int, K: int) -> int:
+    final_H += cardinality_constraint(aux, 5, K)
     final_H += total_investment_constraint(x, 5)
-    print("flag3")
     return final_H
 
 def total_investment_constraint(x: list, p: int) -> int:
     constraint = p * ((100 - sum(x))**2)
     return constraint
 
-def cardinality_constraint(x: list, p: int, K: int):
-    constraint = K
-    count = 0
-    for i in range(0, (len(x) - 7), 7):
-        temp = []
-        for i in range(i, i+7, 1):
-            temp.append(x[i]) 
-        constraint -= or_penalty(temp)
-    constraint = p * (constraint ** 2)
+def cardinality_constraint(aux: list, p: int, K: int):
+    constraint = p * ((K - sum(aux)) ** 2)
     return constraint
 
-def or_penalty(x: list) -> int:
-    if len(x) > 2:
-        temp=[]
-        for i in range(1, len(x)):
-            temp.append(x[i]) 
-        current_penalty = or_penalty(temp)
-        return (x[0] + current_penalty - x[0] * current_penalty)
-    else:
-        return (x[0] + x[1] - (x[0] * x[1]))
 
-def initialise_binary_variables(N: int, initial: list) -> list:
+def initialise_binary_variables(N: int, initial: list, aux: list) -> list:
     x = []
     count = 0
     for i in range(len(initial)):
         if count == 6:
-            x.append(initial[i] * 37)
+            x.append(initial[i] * 37 * aux[(i//7)])
             count = 0
         else:
-            x.append(initial[i] * (2 ** count))
+            x.append(initial[i] * (2 ** count) * aux[(i//7)])
             count = count + 1
+    x = Array(x)
     return x
 
 
